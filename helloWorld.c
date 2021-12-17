34fb061ac8d62613869b8537626b117e03008a61
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2021-12-16 23:39:51 -0500

    Hello

---
+++ helloWorld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Obligatory First Commit
+
+int main(){
+	printf("Hello World!");
+}
+
+// Make executable by writing gcc helloWorld.c -o helloWorld
+// run by writing ./helloWorld.
